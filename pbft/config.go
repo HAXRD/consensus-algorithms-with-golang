752c11c8a9485a3a51f23da8d31a7186354ddc3f
haxrd <xxu.che@gmail.com>
2024-05-24 17:01:13 +0800

    change "config" to const

--- pbft/config.go
+++ pbft/config.go
@@ -1,28 +1,7 @@
 package pbft
 
-import (
-	"gopkg.in/yaml.v3"
-	"os"
+const (
+	TX_THRESHOLD  = 5
+	NUM_OF_NODES  = 3
+	MIN_APPROVALS = 2*(NUM_OF_NODES/3) + 1
 )
-
-type Config struct {
-	TX_THRESHOLD int `yaml:"tx_threshold"` // Maximum num of txs for a block or can exist in the block pool
-	NUM_OF_NODES int `yaml:"num_of_nodes"` // Num of nodes in the system
-	MIN_APPROVAL int // Minimum num of positive votes for the message/block to be valid
-}
-
-func LoadConfig(filename string) (*Config, error) {
-	data, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-
-	var config Config
-	err = yaml.Unmarshal(data, &config)
-	if err != nil {
-		return nil, err
-	}
-	config.MIN_APPROVAL = 2*(config.NUM_OF_NODES/3) + 1
-
-	return &config, nil
-}
